c02a25f176bfddb5371e300f9247c4f51c79f43c
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2022-12-18 17:07:13 +0400

    build api

---
+++ dist/model/boards.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+const subtask = {
+    name: String,
+    isCompleted: Boolean
+};
+const task = {
+    name: String,
+    description: String,
+    status: String,
+    subtasks: [subtask]
+};
+const column = {
+    name: String,
+    tasks: [task]
+};
+const boardsSchema = new mongoose.Schema({
+    userId: String,
+    name: String,
+    columns: [column],
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+});
+const Boards = mongoose.model('Boards', boardsSchema);
+export default Boards;
+//# sourceMappingURL=boards.js.map
---
+++ dist/model/user.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+    username: { type: 'string', required: true },
+    password: { type: 'string', required: true },
+});
+export default mongoose.model('User', userSchema);
+//# sourceMappingURL=user.js.map
